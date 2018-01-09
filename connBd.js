2f915c95442d604c4b6e816ae70aa43b59aaf871
Francisco M. Bravo <franc_consulting@hotmail.com>
2018-01-09 20:35:14 +0100

    actualizado desde ubuntu

--- connBd.js
+++ connBd.js
@@ -3,10 +3,10 @@
 var oracledb = require("oracledb"),
   dbConfig = require("./dbconfig.js"),
   reloj = require("./reloj"),
-  numRows = 100,
-  iRowsAffec = 0,
+  //numRows = 100,
+  //iRowsAffec = 0,
   //arrayData = [],
-  arrayHeader = [],
+  //arrayHeader = [],
   ConnBd = () => {};
 
 oracledb.outFormat = oracledb.ARRAY;
@@ -22,10 +22,13 @@ ConnBd.setRows = filas => {
   numRows = filas;
 };
 
+
 /**
- * Apertura de la conexion a la BD
- * @param {function} cb callback
+ * Apertura de la conexion a la BD.
+ * Devuelve la conexion 
+ * @param {function} cb callback que se ejecuta justo despues de conectar
  */
+
 ConnBd.open = cb => {
   return oracledb.getConnection(
     {
@@ -69,35 +72,14 @@ ConnBd.closeRs = (conn, results) => {
   }
 };
 
+
+
+
 /**
- * Ejecutar una instruccion select
- * @param {Object} conn
- * @param {String} ssql
+ * Ejecutar una instuccion SQL como promesa
+ * @param {Object} conn  conexion con la Bd
+ * @param {String} ssql  instruccion sql a ejecutar
  */
-ConnBd.ejecutarSql = (conn, ssql) => {
-  //console.log(conn);
-  var cab, f, rs, dataRs;
-  var cab = conn.execute(ssql, [], { resultSet: true }, (err, results) => {
-    if (err) {
-      console.error(err.message);
-      ConnBd.closeRs(conn, results);
-      ConnBd.close(conn);
-      return;
-    }
-
-    console.log("   Ejecutando la consulta");
-
-    _getCabecera(conn, results);
-    // console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", numRows);
-
-    // _getFilas(conn, results, numRows);
-    //console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", f);
-    ConnBd.getAllRows(conn, results, numRows).then(data => {
-      console.log(arrayHeader.length, "---", arrayData.length);
-    });
-  });
-};
-
 ConnBd.ejecutarSqlPromise = (conn, ssql) => {
   return new Promise((resolve, reject) => {
     conn.execute(ssql, [], { resultSet: true }, (err, results) => {
@@ -114,25 +96,31 @@ ConnBd.ejecutarSqlPromise = (conn, ssql) => {
   });
 };
 
+/**
+ * Obtener bloques de registros de la consulta realizada. Devuelve un array como promesa con
+ * los registros recuperados.
+ * @param {Object} conn conexion con la Bd
+ * @param {Objeto} results recorset devuelto en la consulta
+ * @param {Integer} numRows cantidad de registros a recuperar por bloqueen cada llamada.
+ *                          Si numRows es null, por defecto se establecen bloques de 250 filas
+ */
 ConnBd.getAllRows = (conn, results, numRows) => {
-  let arrayData = [];
+  let iRowsAffec = 0, 
+      arrayData = [];  //array para almacenar los arrays de registros recuperados en cada bloque
+  numRows = numRows || 250; 
   return new Promise((resolve, reject) => {
     function _getF(conn, results, numRows) {
-      numRows = numRows || 250;
-      results.resultSet.getRows(numRows, function(err, rows) {
+      results.resultSet.getRows(numRows, function(err, rows) { //obtener el bloque de registros
         if (err) {
           //si se produce un error
           console.error(err.message);
-          ConnBd.closeRs(conn, results);
-          // rsClose(conn, results); //cerrar el recordset
+          ConnBd.closeRs(conn, results); //cerrar el recordset
           ConnBd.close(conn); //cerrar la conexion
           reloj.timeStop(); //parar el reloj
           return;
         } else if (rows.length > 0 || iRowsAffec > 0) {
           //cuando hay datos en la consulta o cuando ya hay filas recuperadas
-          console.log(rows.length);
-          arrayData.push(rows); //guardar los datos en un array
-
+          arrayData.push(rows); //guardar el bloque de registros en el array
           iRowsAffec += rows.length; //filas recuperadas
 
           mensaje = " Recibiendo datos ...... recibidos: " + iRowsAffec;
@@ -140,15 +128,15 @@ ConnBd.getAllRows = (conn, results, numRows) => {
           reloj.getMensaje(); //mostrar el mensaje
 
           if (rows.length === numRows) {
-            //comprobacion la ultima vez por si hay mas registros
-            _getF(conn, results, numRows);
+            //realizar una ultima comprobacion por si hay mas registros que el bloque definido
+            _getF(conn, results, numRows); 
           } else {
             //cuando finaliza la recuperacion de datos
             console.log("   Registros recuperados:", iRowsAffec);
-            ConnBd.closeRs(conn, results);
-            ConnBd.close(conn);
-            reloj.timeStop();
-            resolve({ arrayData: arrayData, iRowsAffec: iRowsAffec });
+            ConnBd.closeRs(conn, results); //cerrar el recordset
+            ConnBd.close(conn);   //cerrar la Bd
+            reloj.timeStop(); 
+            resolve({ arrayData: arrayData, iRowsAffec: iRowsAffec }); //devolucion del objeto con los datos y las filas recuperadas
           }
         } else {
           //cuando no hay datos en la consulta
@@ -159,13 +147,20 @@ ConnBd.getAllRows = (conn, results, numRows) => {
         }
       });
     }
-    _getF(conn, results, numRows);
+    _getF(conn, results, numRows); //inicio de la recuperacion de los datos
   });
 };
 
+/**
+ * Obtener los nombre de los campos utilizados en la consulta. Devuelve un array como 
+ * promesa con los nombres de los campos
+ * @param {Objeto} conn conexion a la base de datos
+ * @param {Objeto} results recordset devuleto en la consulta
+ */
 ConnBd.getCabecera = (conn, results) => {
   return new Promise((resolve, reject) => {
-    let cabecera = results.resultSet.metaData;
+    let arrayHeader = [],
+    cabecera = results.resultSet.metaData;
     //console.log(cabecera.length)
 
     arrayHeader = cabecera.map(item => {
@@ -173,149 +168,11 @@ ConnBd.getCabecera = (conn, results) => {
     });
     resolve(arrayHeader);
   });
-
   //console.log(arrayHeader);
 };
 
-ConnBd.getFilas = (conn, results, numRows) => {
-  return new Promise((success, reject) => {
-    results.resultSet.getRows(numRows, function(err, rows) {
-      if (err) {
-        //si se produce un error
-        console.error(err.message);
-        ConnBd.closeRs(conn, results);
-        // rsClose(conn, results); //cerrar el recordset
-        ConnBd.close(conn); //cerrar la conexion
-        reloj.timeStop(); //parar el reloj
-        return;
-      } else if (rows.length > 0 || iRowsAffec > 0) {
-        //cuando hay datos en la consulta o cuando ya hay filas recuperadas
-        console.log(rows.length);
-        arrayData.push(rows); //guardar los datos en un array
 
-        iRowsAffec += rows.length; //filas recuperadas
 
-        mensaje = " Recibiendo datos ...... recibidos: " + iRowsAffec;
-        reloj.setMensaje(mensaje); //definir un nuevo mensaje
-        reloj.getMensaje(); //mostrar el mensaje
 
-        if (rows.length === numRows) {
-          //comprobacion la ultima vez por si hay mas registros
-          ConnBd.getFilas(conn, results, numRows);
-        } else {
-          //cuando finaliza la recuperacion de datos
-          console.log("   Registros recuperados_1:", iRowsAffec);
-          ConnBd.closeRs(conn, results);
-          ConnBd.close(conn);
-          reloj.timeStop();
-          success(arrayData);
-          //TODO:   hacerlo como un metodo independiente donde se le pase Tipo, Nombre Fichero, nombre Hoja, y Array de datos
-          /* ExcelFile.crearLibro("stream", "prueba.xlsx");
-          ExcelFile.crearHoja("miHoja");
-          ExcelFile.getHoja("miHoja");
-          ExcelFile.setCabecera(arrayHeader);
-  
-          let i = 0;
-          arrayData.forEach(element => {
-            element.forEach(e => {
-              ExcelFile.dataControl(e, i, iRowsAffec);
-              i++;
-              if (i == iRowsAffec) {
-                reloj.timeStop();
-              }
-            });
-          });*/
-          //TODO:  Fin
-        }
-      } else {
-        //cuando no hay datos en la consulta
-        console.log("<<<< No hay datos para la consulta planteada >>>>");
-        reloj.timeStop();
-        ConnBd.closeRs(conn, results);
-        ConnBd.close(conn);
-      }
-    });
-  });
-};
-
-/**
- * Ontener el nombre de los campos de la consulta
- * @param {Object} conn
- * @param {array} results
- */
-function _getCabecera(conn, results) {
-  let cabecera = results.resultSet.metaData;
-  //console.log(cabecera.length)
-  return (arrayHeader = cabecera.map(item => {
-    return { header: item.name, key: item.name };
-  }));
-  //console.log(arrayHeader);
-}
-
-/**
- * Recuperar los registros de la consulta
- * @param {*} conn    conexion a la base de datos
- * @param {array} results  registros recuperados
- * @param {integer} numRows numero de registros a recuperar en cada recuperación de datos
- */
-function _getFilas(conn, results, numRows) {
-  results.resultSet.getRows(numRows, function(err, rows) {
-    if (err) {
-      //si se produce un error
-      console.error(err.message);
-      ConnBd.closeRs(conn, results);
-      // rsClose(conn, results); //cerrar el recordset
-      ConnBd.close(conn); //cerrar la conexion
-      reloj.timeStop(); //parar el reloj
-      return;
-    } else if (rows.length > 0 || iRowsAffec > 0) {
-      //cuando hay datos en la consulta o cuando ya hay filas recuperadas
-      console.log(rows.length);
-      arrayData.push(rows); //guardar los datos en un array
-
-      iRowsAffec += rows.length; //filas recuperadas
-
-      mensaje = " Recibiendo datos ...... recibidos: " + iRowsAffec;
-      reloj.setMensaje(mensaje); //definir un nuevo mensaje
-      reloj.getMensaje(); //mostrar el mensaje
-
-      if (rows.length === numRows) {
-        //comprobacion la ultima vez por si hay mas registros
-        _getFilas(conn, results, numRows);
-      } else {
-        //cuando finaliza la recuperacion de datos
-        console.log("   Registros recuperados_1:", iRowsAffec);
-        ConnBd.closeRs(conn, results);
-        ConnBd.close(conn);
-        reloj.timeStop();
-
-        return arrayData;
-        //TODO:   hacerlo como un metodo independiente donde se le pase Tipo, Nombre Fichero, nombre Hoja, y Array de datos
-        /* ExcelFile.crearLibro("stream", "prueba.xlsx");
-        ExcelFile.crearHoja("miHoja");
-        ExcelFile.getHoja("miHoja");
-        ExcelFile.setCabecera(arrayHeader);
-
-        let i = 0;
-        arrayData.forEach(element => {
-          element.forEach(e => {
-            ExcelFile.dataControl(e, i, iRowsAffec);
-            i++;
-            if (i == iRowsAffec) {
-              reloj.timeStop();
-            }
-          });
-        });*/
-        //TODO:  Fin
-      }
-    } else {
-      //cuando no hay datos en la consulta
-      console.log("<<<< No hay datos para la consulta planteada >>>>");
-      reloj.timeStop();
-      ConnBd.closeRs(conn, results);
-      ConnBd.close(conn);
-    }
-  });
-}
 
 module.exports = ConnBd;
